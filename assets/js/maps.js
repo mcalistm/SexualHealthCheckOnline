aa73a07470b9d27ff7fec318e235cd933aec3b49
Maggie McAlister <mcalistm@tcd.ie>
2020-10-19 15:59:18 +0000

    Styled STI/STD section map in maps.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,19 +1,19 @@
 
     function initMap() {
         var map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 3,
+            zoom: 10,
             center: {
-                lat: 46.619261,
-                lng: -33.134766
+                lat: 53.343792,
+                lng: -6.254572
             }
         });
 
         var labels = "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
 
         var locations = [
-            { lat: 40.785091, lng: -73.968285 },
-            { lat: 41.084045, lng: -73.874245 },
-            { lat: 40.754932, lng: -73.984016 },
+            { lat: 53.342150, lng: -6.279260 }, //https://sticlinicdublin.ie/contact/
+            { lat: 53.341020, lng: -6.259620 }, //https://www.trinityclinic.ie/contact-us-1
+            { lat: 53.346920, lng: -6.265770 }, //https://jervismedical.ie/sti-testing-dublin-sexual-health/
         ];
 
         var markers = locations.map(function(location, i) {
